dee5ff273761053832ea1c861c2bd5589e40647e
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-01-18 11:21:08 -0500

    Update Pig.py

--- Pig.py
+++ Pig.py
@@ -1,2 +1,7 @@
 #This program only supports 6-letter words/names
-letter1=input("Enter the first letter: ")
+letter1=input('Enter the first letter: ')
+letter2=input('Enter a letter: ')
+letter3=input('Enter a letter: ')
+letter4=input('Enter a letter: ')
+letter5=input('Enter a letter: ')
+letter6=input('Enter a letter: ')
