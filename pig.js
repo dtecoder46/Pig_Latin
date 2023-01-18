b137dbf22536c51d8458ddede853e2b0e40f585a
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-01-18 16:24:15 -0500

    JavaScript version

---
+++ pig.js
@@ -0,0 +1,9 @@
+println('This program only supports 6-letter words/names')
+var letter1=readLine('Enter the first letter: ');
+var letter2=readLine('Enter a letter: ');
+var letter3=readLine('Enter a letter: ');
+var letter4=readLine('Enter a letter: ');
+var letter5=readLine('Enter a letter: ');
+var letter6=readLine('Enter a letter: ');
+println('Original Word: '+letter1+letter2+letter3+letter4+letter5+letter6);
+println('Translated Word: '+letter2+letter3+letter4+letter5+letter6+'-'+letter1+'ay');
